275f020a880ab167f6f07977d250768b37fdbd9b
xrhoffmann <xrhoffmann@gmail.com>
2021-06-05 08:27:39 +0200

    root_finding.py: Bisection method.

---
+++ mathstuff/root_finding.py
@@ -0,0 +1,63 @@
+"""Functions for finding roots.
+
+2021, Xavier R. Hoffmann <xrhoffmann@gmail.com>
+"""
+
+from typing import Callable, Tuple, Dict
+
+_EPS = 1e-10
+
+
+def bisection(
+    *,
+    x_left: float,
+    x_right: float,
+    func: Callable,
+    func_args: Tuple = (),
+    func_kwargs: Dict = {},
+    eps: float = _EPS,
+) -> float:
+    """Find root with bisection method.
+
+    Args:
+        x_left: Left boundary.
+        x_right: Right boundary.
+        func: Function to evaluate.
+        func_args: Arguments for function.
+        func_kwargs: Keyword arguments for function.
+        eps: Required precision for root.
+
+    Returns:
+        Value of root.
+
+    Raises:
+        ValueError: If left and/or right bounds are roots.
+        ValueError: If function has same sign at left and right bounds.
+    """
+    xl = x_left
+    xr = x_right
+    fl = func(xl, *func_args, **func_kwargs)
+    fr = func(xr, *func_args, **func_kwargs)
+    # check restrictions on bounds
+    if fl == 0:
+        err = "Left bound is a root."
+        raise ValueError(err)
+    elif fr == 0:
+        err = "Right bound is a root."
+        raise ValueError(err)
+    if fl * fr > 0:
+        err = f"Function has same sign at left ({fl}) and right ({fr}) bounds."
+        raise ValueError(err)
+
+    while abs(xr - xl) > eps:
+        x2 = 0.5 * (xl + xr)
+        f2 = func(x2, *func_args, **func_kwargs)
+        if fl * f2 > 0:
+            xl = x2
+            fl = f2
+        elif fr * f2 > 0:
+            xr = x2
+            fr = f2
+        else:
+            break
+    return 0.5 * (xr + xl)
