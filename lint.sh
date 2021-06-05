7a532cf588768193c6db599b9eee0075e9952adf
xrhoffmann <xrhoffmann@gmail.com>
2021-06-05 07:39:03 +0200

    lint.sh: Add linting script.

---
+++ lint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+black mathstuff
+mypy mathstuff
+flake8 mathstuff --ignore=E203,E501,W503 --max-line-length=88 --max-doc-length=72 --max-complexity=15 --select=C,E,F,W,T4
+pydocstyle mathstuff --convention=google
