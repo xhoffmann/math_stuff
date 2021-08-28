9ad85beec66b55b6e4bc4f28dfff1105dff15941
xrhoffmann <xrhoffmann@gmail.com>
2021-08-28 17:57:10 +0200

    histograms.py: Align binning abscissas.

--- mathstuff/histograms.py
+++ mathstuff/histograms.py
@@ -3,6 +3,8 @@
 2019, Xavier R. Hoffmann <xrhoffmann@gmail.com>
 """
 
+from typing import Sequence, Optional
+
 import numpy as np
 
 
@@ -108,3 +110,53 @@ def binning_log(
     if len(bins) > 2 and bins[-1] not in bins_list+x_min and fuse_last_bin:
         bins = np.append(bins[:-2], (bins[-1],))
     return bins
+
+
+
+def binning_align(
+    *, bin_edges: Sequence, x_align: Optional[str] = None, log: bool = False
+) -> np.ndarray:
+    """Computes the bin abscissas given the bin edges.
+
+    Bin abscissas can be 'left', 'center' or 'right' aligned.
+    Default align is 'center' for linear binning.
+    Default align is 'left' for logarithmic binning.
+
+    Args:
+        bin_edges: Array of bin edges, length n.
+        x_align: Abscissas align mode.
+        log: If `True`, binning is logarithmic. If `False`, binning is
+            linear.
+
+    Returns:
+        Array of bin abscissas, length n-1.
+
+    Raises:
+        ValueError: If `x_align` is not None, 'left', 'center'
+            or 'right'.
+    """
+    # default align modes
+    _x_align_default_lin = "center"
+    _x_align_default_log = "left"
+
+    bin_edges = np.array(bin_edges)
+    if x_align is None:
+        if log:
+            x = binning_align(bin_edges=bin_edges, x_align=_x_align_default_log, log=log)
+        else:
+            x = binning_align(bin_edges=bin_edges, x_align=_x_align_default_lin, log=log)
+    elif x_align == "left":
+        x = bin_edges[:-1]
+    elif x_align == "right":
+        x = bin_edges[1:]
+    elif x_align == "center":
+        if log:
+            x = bin_edges[:-1] * np.sqrt(bin_edges[1:] / bin_edges[:-1])
+        else:
+            x = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+    else:
+        err = f"x_align mode ({x_align}) is not valid."
+        raise ValueError(err)
+    return x
+
+
