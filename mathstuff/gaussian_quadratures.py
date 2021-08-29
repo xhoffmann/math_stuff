ab5bb2c170244d16f891864e36776f470955ce0d
Xavier Roderic Hoffmann <xrhoffmann@gmail.com>
2021-08-29 16:06:53 +0200

    gaussian_quadratures.py: Function to integrate with Legendre polynomials.

--- mathstuff/gaussian_quadratures.py
+++ mathstuff/gaussian_quadratures.py
@@ -5,6 +5,8 @@
 
 import numpy as np
 
+from mathstuff import root_finding
+
 from typing import Callable, Tuple
 
 
@@ -31,7 +33,7 @@ def legendre_polynomial(x: float, n: int) -> float:
 
 
 def find_bisection_bounds(
-    n: int, polynomial: Callable
+    *, n: int, polynomial: Callable
 ) -> Tuple[Tuple[float, float], ...]:
     """Find root bounds for n-order polynomial.
 
@@ -46,10 +48,44 @@ def find_bisection_bounds(
     while True:
         bounds = np.linspace(-1, 1, k * n + 1)
         values = np.array([polynomial(x, n) for x in bounds])
-        left = values[:-1]
-        right = values[1:]
-        mask = left * right < 0
+        mask = values[:-1] * values[1:] < 0
         if sum(mask) == n:
-            return tuple(x for x in zip(left[mask], right[mask]))
+            return tuple(x for x in zip(bounds[:-1][mask], bounds[1:][mask]))
         else:
             k += 1
+
+
+def integrate_legendre(func: Callable, n: int, a: float, b: float) -> float:
+    """Integrate with Legendre polynomials of order n.
+
+    Args:
+        func: Function to integrate.
+        n: Polynomial order.
+        a: Left bound of integration interval.
+        b: Right bound of integration interval.
+
+    Returns:
+        Value of integral.
+    """
+    # find bisection bounds of polynomial roots
+    bounds = find_bisection_bounds(n=n, polynomial=legendre_polynomial)
+    # find roots of polynomial
+    roots = [
+        root_finding.hybrid_secant_bisection(
+            x_left=bound[0],
+            x_right=bound[1],
+            func=legendre_polynomial,
+            func_args=(n,),
+        )
+        for bound in bounds
+    ]
+    # compute integration weights
+    weights = np.array(
+        [
+            2.0 * (1.0 - root ** 2) / ((n * legendre_polynomial(root, n - 1)) ** 2)
+            for root in roots
+        ]
+    )
+    # compute function values
+    func_values = np.array([func(0.5 * ((b - a) * root + a + b)) for root in roots])
+    return 0.5 * (b - a) * sum(weights * func_values)
